53bdf0aba4249a7c9ce7311270b9b8b505bed786
vaais <shahidlatifkcs786@gmail.com>
2024-05-23 17:14:07 +0500

    After map and hero image is include

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ close_btn.addEventListener("click", function () {
   close_btn.classList.toggle("hidden");
   nav.style.transform = "translateX(-100%)";
 });
+
+let map = L.map("map").setView([35.2889769, 75.640937], 15);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([35.2889769, 75.640937])
+  .addTo(map)
+  .bindPopup("ADVENTURE SARAI HOTEL SKARDU")
+  .openPopup();
